830505fbfa687d0de18286f9fdd7af60472a047f
Sazzad Hossain <sazzadhrz@yahoo.com>
2020-01-14 00:20:14 +0600

    created object oriented models for teacher and student

--- portal/classroom/models.py
+++ portal/classroom/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.EmailField()
+    department = models.CharField()
+    mobile = models.IntegerField()
+
+
+class Teacher(Person):
+    initial = models.CharField(max_length=4)
+
+
+class Student(Person):
+    s_id = models.IntegerField(max_length=10)
